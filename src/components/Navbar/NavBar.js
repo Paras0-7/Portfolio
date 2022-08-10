cf49748fd73263f87996a66377c0867900a3e544
Paras0-7 <parasrawat937@gmail.com>
2022-08-10 13:11:58 +0530

    nav-bar

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,54 @@
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+export const NavBar = function () {
+  return (
+    <nav className="navbar">
+      <div className="nav-sub-container">
+        <div className="nav-blk"></div>
+        <div className="nav-title-container">
+          <Link to="/">
+            <h2 className="nav-name">Paras Rawat</h2>
+          </Link>
+          <p className="slash">/</p>
+          <p className="nav-designation">Software Developer</p>
+        </div>
+        <div className="nav-link-container">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "nav-links"
+            }
+          >
+            About Me
+          </NavLink>
+          <NavLink
+            to="/resume"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "nav-links"
+            }
+          >
+            Resume
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "nav-links"
+            }
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "nav-links"
+            }
+          >
+            Contact
+          </NavLink>
+        </div>
+
+        {/* <div className={`mobile-nav ${menuOpen?}`} */}
+      </div>
+    </nav>
+  );
+};
