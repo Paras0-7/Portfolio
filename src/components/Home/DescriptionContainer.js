64f20aedfe4687a03601ecb8685f8b108072e25e
Paras0-7 <parasrawat937@gmail.com>
2023-11-10 23:23:35 +0530

    portfolio update

--- src/components/Home/DescriptionContainer.js
+++ src/components/Home/DescriptionContainer.js
@@ -6,11 +6,7 @@ export const DescriptionContainer = function () {
       <div className="headline-1">Hello</div>
       <div className="headline-2">Here's who I am & what I do</div>
       <div className="btn-container">
-        <a
-          href="https://drive.google.com/file/d/10jRTyYZxiq6qaBptbdYGxD0iaR0BqoEN/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://drive.google.com/file/d/1dj5QyNQkmxaRehBgQop-de9yfRBg99fY/view?usp=drive_link" target="_blank" rel="noreferrer">
           <button className="resume-btn">Resume</button>
         </a>
         <Link to="/projects">
@@ -18,11 +14,7 @@ export const DescriptionContainer = function () {
         </Link>
       </div>
 
-      <div className="bio-paragraph">
-        I'm an engineering graduate and a programmer. I work as a Web Developer
-        with experience in handling JavaScript, TypeScript, Node.js, and
-        React.js coding.
-      </div>
+      <div className="bio-paragraph">I'm an engineering graduate and a programmer. I work as a Web Developer with experience in handling JavaScript, TypeScript, Node.js, and React.js coding.</div>
       <div className="bio-paragraph"></div>
     </div>
   );
