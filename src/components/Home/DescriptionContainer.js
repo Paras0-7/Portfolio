12b2a34579d5d0be2b83db6fcad47fcb4a394f76
Paras Rawat <63183825+Paras0-7@users.noreply.github.com>
2024-09-21 19:48:26 +0530

    Update DescriptionContainer.js

--- src/components/Home/DescriptionContainer.js
+++ src/components/Home/DescriptionContainer.js
@@ -6,7 +6,7 @@ export const DescriptionContainer = function () {
       <div className="headline-1">Hello</div>
       <div className="headline-2">Here's who I am & what I do</div>
       <div className="btn-container">
-        <a href="https://drive.google.com/file/d/1tcLHD2bDWtuAgA8eMMBwiP3M5Dv0pZSd/view?usp=sharing" target="_blank" rel="noreferrer">
+        <a href="https://drive.google.com/file/d/1oUnwWQc0iumMR04p5ddpmJQqBptu1jmB/view?usp=sharing" target="_blank" rel="noreferrer">
           <button className="resume-btn">Resume</button>
         </a>
         <Link to="/projects">
